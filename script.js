e32136bb24cebcf05c7be8dc6a43620f53714204
Adonay Mendez <adonay.mendez255@gmail.com>
2026-06-22 22:27:30 -0400

    add a conditional statment to change status img accordingly

--- script.js
+++ script.js
@@ -5,5 +5,13 @@ buttons.forEach((button) => {
     const answer = button.closest(".faq-item").querySelector('.answer'); 
 
     answer.classList.toggle("show"); 
+
+    const img = button.querySelector("img");
+
+    if(answer.classList.contains("show")){
+      img.src = "assets/images/icon-minus.svg"; 
+    } else {
+      img.src = "assets/images/icon-plus.svg";
+    }
   });
 });
